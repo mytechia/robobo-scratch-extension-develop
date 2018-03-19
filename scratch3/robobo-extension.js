2359b0435fcf36191579f8f07bb6578a22665e05
Shiul93 <lfllamas93@gmail.com>
2018-03-19 12:44:00 +0100

    Testing another way to load

--- scratch3/robobo-extension.js
+++ scratch3/robobo-extension.js
@@ -6,7 +6,24 @@ var RoboboExtension = function () {
 
 //var rem = undefined; //remote connection to the robot
 
+function getScript(source, callback) {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
 
+    script.onload = script.onreadystatechange = function( _, isAbort ) {
+        if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+            script.onload = script.onreadystatechange = null;
+            script = undefined;
+
+            if(!isAbort) { if(callback) callback(); }
+        }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+}
+getScript("https://mytechia.github.io/robobo-scratch-extension-develop/develop/remote-library/remotelib.js",()=>{});
 /**
  * @return {object} This extension's metadata.
  */
